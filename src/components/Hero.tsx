5c5fd1038f02808a5192cea107e5e95a879060d4
milesthedisch <milesthedisch@gmail.com>
2022-06-19 23:08:35 -0700

    Minimal build

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,26 @@
-import { Flex, Heading } from '@chakra-ui/react'
+import { Flex, Heading } from "@chakra-ui/react";
 
-export const Hero = ({ title }: { title: string }) => (
-  <Flex
-    justifyContent="center"
-    alignItems="center"
-    height="100vh"
-    bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
-    bgClip="text"
-  >
-    <Heading fontSize="6vw">{title}</Heading>
-  </Flex>
-)
+export const Hero = ({
+  title,
+  subheading,
+}: {
+  title: string;
+  subheading: string;
+}) => (
+  <>
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
+      bgClip="text"
+    >
+      <Heading fontSize="6vw">{title}</Heading>
+    </Flex>
+  </>
+);
 
 Hero.defaultProps = {
-  title: 'with-chakra-ui-typescript',
-}
+  title: "Miles Disch",
+  subheading: "hello 💻",
+};
